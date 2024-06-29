3ec17bfc2bb81b2ccf8268c0f4bafe4fd588f5b0
Fábio Machado de Oliveira <machado2@gmail.com>
2024-06-28 23:18:18 -0300

    Umami analytics

--- ui/src/index.tsx
+++ ui/src/index.tsx
@@ -5,12 +5,14 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+    document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <script defer src="https://umami.fbmac.net/script.js"
+                data-website-id="a97afffa-a21b-42a8-a77d-228396f6b8ae"></script>
+        <App/>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
